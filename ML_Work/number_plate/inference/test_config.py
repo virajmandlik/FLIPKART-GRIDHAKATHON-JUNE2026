1bfb2285723920dd3394a1f57e6f33ad7c1f109e
vmandlik_cisco <vmandlik+cisco@cisco.com>
2026-06-24 00:42:06 +0530

    Added the files for research paper

---
+++ ML_Work/number_plate/inference/test_config.py
@@ -0,0 +1,8 @@
+from ML_Work.number_plate.inference.config import *
+
+print("Project:", PROJECT_ROOT)
+print("Number Plate Model:", NUMBER_PLATE_MODEL)
+print("Helmet Model:", HELMET_MODEL)
+print("Input Folder:", INPUT_FOLDER)
+print("Output Folder:", OUTPUT_FOLDER)
+print("Cropped Folder:", CROPPED_FOLDER)
