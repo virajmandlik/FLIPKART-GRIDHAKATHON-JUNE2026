1230d9e6bf492401d9ae49c17f5b183c4832274e
vmandlik_cisco <vmandlik+cisco@cisco.com>
2026-06-24 00:09:50 +0530

    Added the changed files

--- number_plate/inference/test_config.py
+++
@@ -1,8 +0,0 @@
-from inference.config import *
-
-print("Project:", PROJECT_ROOT)
-print("Number Plate Model:", NUMBER_PLATE_MODEL)
-print("Helmet Model:", HELMET_MODEL)
-print("Input Folder:", INPUT_FOLDER)
-print("Output Folder:", OUTPUT_FOLDER)
-print("Cropped Folder:", CROPPED_FOLDER)
